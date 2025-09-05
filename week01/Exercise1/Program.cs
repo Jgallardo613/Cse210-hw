72dddea2ad7ff6094f66e089567b9d893bcda007
Jgallardo613 <Jgallardo1@byupathway.edu>
2025-09-04 23:48:52 -0600

    program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,24 @@
-using System;
+using System; // This lines 
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Line 7: This line prompts the user to enter their first name.
+        Console.Write("What is your first name?");
+
+        //Line 10: This line reads the user input and stores it in a string variable named'firstname'.
+        string firstName = Console.ReadLine();
+
+        //Line 13: This line prompts the user to enter their last name?);
+        Console.Write("What is your last name? ");
+
+        //line 16: This line reads the user input stores its in a string variable named "LastName".
+        string LastName = Console.ReadLine();
+
+        //line 19: This line prints the final formatted sentence using the string interpolation.
+        Console.WriteLine($"Your name is {LastName}, {firstName}, {LastName}.");
+        
+
     }
 }
