34dafa4fa3b06148a4a5dc0f0c20d05a1d9793b0
Jgallardo613 <Jgallardo1@byupathway.edu>
2025-10-13 23:40:12 -0600

    Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running runningActivity = new Running("03 Nov 2025", 30, 3.0);
+        activities.Add(runningActivity);
+
+        Cycling cyclingActivity = new Cycling("04 Nov 2025", 45, 15.0);
+        activities.Add(cyclingActivity);
+
+        Swimming swimmingActivity = new Swimming("05 Nov 2025", 60, 20);
+        activities.Add(swimmingActivity);
+
+        Console.WriteLine("--- Exercise Tracking Summary ---");
+        Console.WriteLine();
+        
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+            Console.WriteLine(); 
+        }
+
+        Console.WriteLine("--- Report Complete ---");
     }
 }
